b10ec5947caf0b11b2f5d28bd5d4bf60a5f8a8fe
ianfajardo <ianfajardozx@gmail.com>
2020-03-14 11:02:41 -0400

    update package depdencies and copy index into dest

--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,77 @@
-const path = require('path');
- const MiniCssExtractPlugin = require("mini-css-extract-plugin");
- const webpack = require('webpack');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
+const webpackCopy = require("copy-webpack-plugin");
 
- const extractSass = new MiniCssExtractPlugin({
-   filename: "[name].min.css",
-   chunkFilename: "[id].css"
- });
+const extractSass = new MiniCssExtractPlugin({
+  filename: "[name].min.css",
+  chunkFilename: "[id].css"
+});
 
- var envmode = 'development';
+var envmode = "development";
 
- if (process.env.NODE_ENV === 'production') {
-   envmode = 'production';
-   console.log('production build');
- }
+if (process.env.NODE_ENV === "production") {
+  envmode = "production";
+  console.log("production build");
+}
 
- module.exports = [{
-   mode: envmode,
-   entry: {
-     main: './public/js/main.js'
-   },
-   output: {
-     filename: '[name].min.js',
-     path: path.resolve(__dirname, 'dest')
-   },
-   devServer: {
-     contentBase: path.join(__dirname, 'public'),
-     watchContentBase: true,
-     hot: true,
-     overlay: true
-   },
-   module: {
-     rules: [{
-         test: /\.js$/,
-         exclude: /(node_modules|bower_components)/,
-         use: {
-           loader: 'babel-loader',
-           options: {
-             minified: false,
-             compact: false
-           }
-         }
-       },
-       {
-         test: /\.(sa|sc|c)ss$/,
-         use: [
-           MiniCssExtractPlugin.loader,
-           'css-loader',
-           'sass-loader',
-         ],
-       }
-     ]
-   },
-   resolve: {
-     alias: {
-       'masonry': 'masonry-layout',
-       'isotope': 'isotope-layout',
-       'vue': 'vue/dist/vue.js'
-     }
-   },
-   plugins: [
-     new webpack.ProvidePlugin({
-       $: 'jquery',
-       jquery: 'jquery',
-       'window.jQuery': 'jquery',
-       jQuery: 'jquery'
-     }),
-      /*new HtmlWebpackPlugin({
-        title: 'Development',
-        template: './public/index.html'
-      }),*/
-     new webpack.HotModuleReplacementPlugin(),
-     extractSass
-   ]
- }];
+module.exports = [
+  {
+    mode: envmode,
+    entry: {
+      main: "./public/js/main.js"
+    },
+    output: {
+      filename: "[name].min.js",
+      path: path.resolve(__dirname, "dest")
+    },
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      watchContentBase: true,
+      hot: true,
+      overlay: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              minified: false,
+              compact: false
+            }
+          }
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+        }
+      ]
+    },
+    resolve: {
+      alias: {
+        masonry: "masonry-layout",
+        isotope: "isotope-layout",
+        vue: "vue/dist/vue.js"
+      }
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jquery: "jquery",
+        "window.jQuery": "jquery",
+        jQuery: "jquery"
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+      extractSass,
+      new webpackCopy([
+        {
+          from: path.resolve(__dirname, "public/index.html"),
+          to: path.resolve(__dirname, "dest")
+        }
+      ])
+    ]
+  }
+];
